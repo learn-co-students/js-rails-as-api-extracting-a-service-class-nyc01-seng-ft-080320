2e44ba74241452870947182741bbd61b741d8eda
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-09-20 00:49:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
 end
+
+# it is possible for a single controller#action to render data from multiple models 
+# it is possible to specify what we want + dont want to render
+
